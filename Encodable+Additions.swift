99866fcc1d932bb0c7e78bf0870676fbe71e2e4f
Shashank <34152717+iThink32@users.noreply.github.com>
2019-01-18 16:32:24 +0530

    Update Encodable+Additions.swift

--- Encodable+Additions.swift
+++ Encodable+Additions.swift
@@ -3,21 +3,20 @@
 //  SampleAppCustomui
 //
 //  Created by N.A Shashank on 18/01/19.
-//  Copyright © 2019 Razorpay. All rights reserved.
 //
 
 import UIKit
 
-//extension Decodable {
-//    
-//    init?(dict:[String:Any]) {
-//        guard let data = try? JSONSerialization.data(withJSONObject: dict, options: JSONSerialization.WritingOptions.prettyPrinted), let decodable = try?  JSONDecoder().decode(Self.self, from: data) else{
-//           return nil
-//        }
-//        self = decodable
-//    }
-//    
-//}
+extension Decodable {
+   
+   init?(dict:[String:Any]) {
+       guard let data = try? JSONSerialization.data(withJSONObject: dict, options: JSONSerialization.WritingOptions.prettyPrinted), let decodable = try?  JSONDecoder().decode(Self.self, from: data) else{
+          return nil
+       }
+       self = decodable
+   }
+   
+}
 
 extension Encodable {
     
